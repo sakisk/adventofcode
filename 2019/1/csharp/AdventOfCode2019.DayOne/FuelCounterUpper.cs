5409f888204a527a8817fe025c8bf85f99452984
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-01 11:19:29 +0000

    Day1.1-c#: Add csharp projects with a failing test

---
+++ 2019/1/csharp/AdventOfCode2019.DayOne/FuelCounterUpper.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2019.DayOne
+{
+
+    public class FuelCounterUpper
+    {
+        private readonly int _mass;
+
+        public FuelCounterUpper(int mass)
+        {
+            _mass = mass;
+        }
+
+        public FuelCounterUpper DivideByThree()
+        {
+            return this;
+        }
+
+        public int Compute()
+        {
+            return 0;
+        }
+    }
+}
---
+++ 2019/1/csharp/AdventOfCode2019.DayOneTests/FuelCounterUpperTests.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2019.DayOne;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayOneTests
+{
+    public class FuelCounterUpperTests 
+    {
+        [Fact]
+        public void ShouldDivideByThree()
+        {
+            new FuelCounterUpper(12).DivideByThree().Compute().Should().Be(4);
+        }
+    }
+}
