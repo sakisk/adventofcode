f2cbd1a27e9628a7f7e3d723697ee0394f50d364
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-02 19:13:07 +0000

    Day2.1-c#: Add support to process multiply operand and process intcode sequence with all operations untli halt is processed

--- 2019/2/csharp/AdventOfCode2019.DayTwo/IntcodeProcessor.cs
+++ 2019/2/csharp/AdventOfCode2019.DayTwo/IntcodeProcessor.cs
@@ -5,23 +5,27 @@ namespace AdventOfCode2019.DayTwo
 {
     public class IntcodeProcessor
     {
+        private const int HaltOpcode = 99;
         private readonly IList<int> _input;
         private int _index;
+        public bool ShouldHalt { get; }
 
         public static IntcodeProcessor Create(string input) => new IntcodeProcessor(input.Split(',').Select(int.Parse).ToList(), 0);
-        private IntcodeProcessor(IList<int> input, int index)
+        private IntcodeProcessor(IList<int> input, int index, bool shouldHalt = false)
         {
             _input = input;
             _index = index;
+            ShouldHalt = shouldHalt;
         }
 
         public IntcodeProcessor Process()
         {
             var opcode = _input[_index];
 
-            if (opcode == 99)
+
+            if (opcode == HaltOpcode)
             {
-                return new IntcodeProcessor(_input, 0);
+                return new IntcodeProcessor(_input, 0, shouldHalt: true);
             }
 
             if (opcode == 1)
@@ -36,10 +40,33 @@ namespace AdventOfCode2019.DayTwo
                 return new IntcodeProcessor(_input, _index);
             }
 
+            if (opcode == 2)
+            {
+                var firstOperandIndex = _input[_index + 1];
+                var secondOperandIndex = _input[_index + 2];
+                var resultIndex = _input[_index + 3];
+
+                _input[resultIndex] = _input[firstOperandIndex] * _input[secondOperandIndex];
+                _index += 4;
+
+                return new IntcodeProcessor(_input, _index);
+            }
+
             return new IntcodeProcessor(_input, _index);
         }
 
-
         public string Result => string.Join(",", _input.Select(x => x.ToString()).ToArray());
+
+        public IntcodeProcessor ProcessUntilHalt()
+        {
+            var processor = this;
+
+            while (!processor.ShouldHalt)
+            {
+                processor = Process();
+            }
+
+            return processor;
+        }
     }
 }
--- 2019/2/csharp/AdventOfCode2019.DayTwoTests/IntcodeProcessorTests.cs
+++ 2019/2/csharp/AdventOfCode2019.DayTwoTests/IntcodeProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using AdventOfCode2019.DayTwo;
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode2019.DayTwo;
 using FluentAssertions;
 using Xunit;
 
@@ -7,7 +9,6 @@ namespace AdventOfCode2019.DayTwoTests
     public class IntcodeProcessorTests
     {
         [Theory]
-        [InlineData("99", "99")]
         [InlineData("99,1,0,0,99", "99,1,0,0,99")]
         public void ShouldProcessProgramHaltOpcode(string input, string result) =>
             IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
@@ -17,5 +18,25 @@ namespace AdventOfCode2019.DayTwoTests
         public void GivenAnAdditionOpcode_WhenProcessed_ShouldAddNextTwoIntsAndStoreResultInPositionIndicatedByThirdInt(
             string input, string result) =>
             IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
+
+        [Theory]
+        [InlineData("2,0,0,3,99", "2,0,0,4,99")]
+        [InlineData("2,3,0,3,99", "2,3,0,6,99")]
+        [InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        public void GivenAMultiplicationOpcode_WhenProcessed_ShouldMultiplyNextTwoIntsAndStoreResultInPositionIndicatedByThirdInt(
+            string input, string result) =>
+            IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
+
+        [Theory]
+        [InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void GivenAnIntcodeSequence_WhenProcessed_ShouldProcessOpcodesUntilHalt(string input, string result) => 
+            IntcodeProcessor.Create(input).ProcessUntilHalt().Result.Should().Be(result);
+
+        [Fact]
+        public void ShouldProcessPuzzleInput() => 
+            IntcodeProcessor
+                .Create(File.ReadAllText("input"))
+                .ProcessUntilHalt()
+                .Result.Split(',').First().Should().Be("8017076");
     }
 }
