58f19e77c24e5975c5ab0db42d3e21483b797319
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-02 11:09:04 +0000

    Day2.1-c#: Add a failing test for processing halt program opcode

---
+++ 2019/2/csharp/AdventOfCode2019.DayTwo/IntcodeProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdventOfCode2019.DayTwo
+{
+    public class IntcodeProcessor
+    {
+        public IntcodeProcessor(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntcodeProcessor Process()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Result() => throw new NotImplementedException();
+    }
+}
---
+++ 2019/2/csharp/AdventOfCode2019.DayTwoTests/IntcodeProcessorTests.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2019.DayTwo;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayTwoTests
+{
+    public class IntcodeProcessorTests
+    {
+        [Theory]
+        [InlineData("99", "99")]
+        [InlineData("99,1,0,0,99", "99,1,0,0,99")]
+        public void ShouldProcessProgramHaltOpcode(string input, string result) =>
+            new IntcodeProcessor(input).Process().Should().Be(result);
+    }
+}
