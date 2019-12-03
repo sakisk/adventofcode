df26c9c62ebd30ed02d6585e7cdac80bc08fac68
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-03 01:00:38 +0000

    Day2.2-c#: ALL THE THINGS STATIC

--- 2019/2/csharp/AdventOfCode2019.DayTwo/IntcodeProcessor.cs
+++ 2019/2/csharp/AdventOfCode2019.DayTwo/IntcodeProcessor.cs
@@ -9,84 +9,56 @@ namespace AdventOfCode2019.DayTwo
         private const int HaltOpcode = 99;
         private const int AddOpcode = 1;
         private const int MultiplyOpcode = 2;
-        private readonly IList<int> _input;
-        private int _index;
 
+        public static string Process(string input) => Process(InputToIntcode(input));
 
-        public static IntcodeProcessor Create(string input) => new IntcodeProcessor(input.Split(',').Select(int.Parse).ToList(), 0);
-        private IntcodeProcessor(IList<int> input, int index, bool shouldHalt = false)
+        public static int FindNounAndVerbForInstruction(string input, int targetInstruction) => FindNounAndVerbForInstruction(InputToIntcode(input), targetInstruction);
+
+        private static string Process(IList<int> input)
         {
-            _input = input;
-            _index = index;
-            ShouldHalt = shouldHalt;
+            var index = 0;
+
+            while (true)
+            {
+                if (input[index] == HaltOpcode)
+                {
+                    return IntcodeToString(input);
+                }
+
+                if (input[index] == AddOpcode)
+                {
+                    ProcessOperand(input, index, (first, second) => first + second);
+                    index += 4;
+                }
+                else if (input[index] == MultiplyOpcode)
+                {
+                    ProcessOperand(input, index, (first, second) => first * second);
+                    index += 4;
+                }
+            }
         }
 
-        public IntcodeProcessor Process(IList<int> input)
+        private static int FindNounAndVerbForInstruction(IEnumerable<int> input, int targetInstruction)
         {
-            var opcode = input[_index];
-
-            if (opcode == HaltOpcode)
-            {
-                return new IntcodeProcessor(input, 0, shouldHalt: true);
-            }
-
-            if (opcode == AddOpcode)
-            {
-                ProcessOperand(input, _index, (first, second) => first + second);
-                MoveToNextOperand();
-
-                return new IntcodeProcessor(input, _index);
-            }
-
-            if (opcode == MultiplyOpcode)
-            {
-                ProcessOperand(input, _index, (first, second) => first * second);
-                MoveToNextOperand();
-
-                return new IntcodeProcessor(input, _index);
-            }
-
-            return new IntcodeProcessor(input, _index);
-
-            void MoveToNextOperand() => _index += 4;
-        }
-
-        private static void ProcessOperand(IList<int> input, int index, Func<int, int, int> operation) =>
-            input[input[index + 3]] = operation(input[input[index + 1]], input[input[index + 2]]);
-
-        public IntcodeProcessor Process() => Process(_input);
-
-        public string Result => string.Join(",", _input.Select(x => x.ToString()).ToArray());
-
-        public IntcodeProcessor ProcessUntilHalt()
-        {
-            var processor = this;
-
-            while (!processor.ShouldHalt)
-            {
-                processor = Process();
-            }
-
-            return processor;
-        }
-
-        public int FindNounAndVerbForInstruction(int instruction)
-        {
-            var noun = instruction / 614400 - 1;
+            var noun = targetInstruction / 614400 - 1;
             var verb = 0;
-            var test = _input.ToList();
+            var test = input.ToList();
 
             test[1] = noun;
             test[2] = verb;
 
-            var processor = new IntcodeProcessor(test, 0);
+            Process(test);
 
-            verb = instruction - processor.ProcessUntilHalt().Instruction;
+            verb = targetInstruction - test.First();
 
             return 100 * noun + verb;
         }
+        private static string IntcodeToString(IList<int> input) => string.Join(",", input.Select(x => x.ToString()).ToArray());
 
-        public bool ShouldHalt { get; }
-        public int Instruction => _input.First();
+        private static IList<int> InputToIntcode(string input) => input.Split(',').Select(int.Parse).ToList();
+
+        private static void ProcessOperand(IList<int> input, int index, Func<int, int, int> operation) =>
+            input[input[index + 3]] = operation(input[input[index + 1]], input[input[index + 2]]);
+
     }
 }
--- 2019/2/csharp/AdventOfCode2019.DayTwoTests/IntcodeProcessorTests.cs
+++ 2019/2/csharp/AdventOfCode2019.DayTwoTests/IntcodeProcessorTests.cs
@@ -10,14 +10,13 @@ namespace AdventOfCode2019.DayTwoTests
     {
         [Theory]
         [InlineData("99,1,0,0,99", "99,1,0,0,99")]
-        public void ShouldProcessProgramHaltOpcode(string input, string result) =>
-            IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
+        public void ShouldProcessProgramHaltOpcode(string input, string result) => IntcodeProcessor.Process(input).Should().Be(result);
 
         [Theory]
         [InlineData("1,0,0,3,99", "1,0,0,2,99")]
         public void GivenAnAdditionOpcode_WhenProcessed_ShouldAddNextTwoIntsAndStoreResultInPositionIndicatedByThirdInt(
             string input, string result) =>
-            IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
+            IntcodeProcessor.Process(input).Should().Be(result);
 
         [Theory]
         [InlineData("2,0,0,3,99", "2,0,0,4,99")]
@@ -25,12 +24,12 @@ namespace AdventOfCode2019.DayTwoTests
         [InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")]
         public void GivenAMultiplicationOpcode_WhenProcessed_ShouldMultiplyNextTwoIntsAndStoreResultInPositionIndicatedByThirdInt(
             string input, string result) =>
-            IntcodeProcessor.Create(input).Process().Result.Should().Be(result);
+            IntcodeProcessor.Process(input).Should().Be(result);
 
         [Theory]
         [InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
         public void GivenAnIntcodeSequence_WhenProcessed_ShouldProcessOpcodesUntilHalt(string input, string result) => 
-            IntcodeProcessor.Create(input).ProcessUntilHalt().Result.Should().Be(result);
+            IntcodeProcessor.Process(input).Should().Be(result);
 
         [Fact]
         public void ShouldProcessInputTo1202ProgramAlarm()
@@ -42,14 +41,14 @@ namespace AdventOfCode2019.DayTwoTests
 
             var input1202ProgramAlarm = string.Join(',', input);
 
-            IntcodeProcessor
-                .Create(input1202ProgramAlarm)
-                .ProcessUntilHalt()
-                .Result.Split(',').First().Should().Be("8017076");
+            IntcodeProcessor.Process(input1202ProgramAlarm).Split(',').First().Should().Be("8017076");
         }
 
         [Fact]
-        public void ShouldProcessInputUntilCorrectPointerHasCorrectInstruction() => 
-            IntcodeProcessor.Create(File.ReadAllText("input")).FindNounAndVerbForInstruction(19690720).Should().Be(3146);
+        public void ShouldProcessInputUntilCorrectPointerHasCorrectInstruction()
+        {
+            var input = File.ReadAllText("input");
+            IntcodeProcessor.FindNounAndVerbForInstruction(input, 19690720).Should().Be(3146);
+        }
     }
 }
