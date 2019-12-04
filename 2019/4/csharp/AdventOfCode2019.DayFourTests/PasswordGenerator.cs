ee4ca0836787e0d9f3c4fd59ab2841967140516e
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-04 17:16:53 +0000

    Day4.1-c#: Add Part 1 solution

---
+++ 2019/4/csharp/AdventOfCode2019.DayFourTests/PasswordGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2019.DayFourTests
+{
+    public class PasswordGenerator
+    {
+        public static int Next(int start)
+        {
+            while (true)
+            {
+                if (IsValidPassword(++start)) return start;
+            }
+        }
+
+        public static bool IsValidPassword(int password)
+        {
+            var adjacentPairs = GetAdjacentPairs(password);
+            return HasSixDigits(password) && HasAtLeastADouble(adjacentPairs) && DigitsIncreaseFromLeftToRight(adjacentPairs);
+        }
+
+        public static IEnumerable<int> PasswordsInRange(Range range)
+        {
+            var nextPassword = range.Start.Value;
+
+            if (IsValidPassword(nextPassword))
+                yield return nextPassword;
+
+            if (IsValidPassword(range.End.Value))
+                yield return range.End.Value;
+
+            while (nextPassword < range.End.Value)
+            {
+                nextPassword = Next(nextPassword);
+                if (nextPassword < range.End.Value)
+                    yield return nextPassword;
+            }
+        }
+
+        private static bool HasSixDigits(int password) => password >= 111111;
+
+        private static bool HasAtLeastADouble(IEnumerable<(char, char)> pairs) => pairs.Any(x => x.Item1 == x.Item2);
+
+        private static bool DigitsIncreaseFromLeftToRight(IEnumerable<(char, char)> pairs) => !pairs.Any(x => x.Item1 > x.Item2);
+
+        private static IList<(char, char)> GetAdjacentPairs(int password)
+        {
+            var digits = password.ToString().ToCharArray();
+
+            return digits.Zip(digits.Skip(1), (first, second) => (first, second)).ToList();
+        }
+    }
+}
---
+++ 2019/4/csharp/AdventOfCode2019.DayFourTests/PasswordGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayFourTests
+{
+    public class PasswordGeneratorTests
+    {
+        [Theory]
+        [InlineData(111111, true)]
+        [InlineData(223450, false)]
+        [InlineData(123789, false)]
+        public void ShouldValidatePassword(int password, bool valid) => PasswordGenerator.IsValidPassword(password).Should().Be(valid);
+
+        [Theory]
+        [InlineData(111111, 111112)]
+        [InlineData(111119, 111122)]
+        [InlineData(111199, 111222)]
+        public void ShouldGenerateNextValidPassword(int seed, int next) => PasswordGenerator.Next(seed).Should().Be(next);
+
+        [Theory]
+        [InlineData(111111, 111111, 1)]
+        [InlineData(111111, 111112, 2)]
+        [InlineData(137683, 596253, 1864)]
+        public void ShouldGenerateValidPasswordsInRange(int start, int end, int count) =>
+            PasswordGenerator.PasswordsInRange(new Range(start, end)).Distinct().Count().Should().Be(count);
+    }
+}
