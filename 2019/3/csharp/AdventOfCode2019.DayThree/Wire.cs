6794a63f69c5b4cb86ac97ce13cb0ee70e6f7f64
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-04 02:41:44 +0000

    Day3.2-c#: Refactor with switch expressions

--- 2019/3/csharp/AdventOfCode2019.DayThree/Wire.cs
+++ 2019/3/csharp/AdventOfCode2019.DayThree/Wire.cs
@@ -7,74 +7,100 @@ namespace AdventOfCode2019.DayThree
     public class Wire
     {
         private readonly (int, int) _centralPort = (0, 0);
+        private const char Right = 'R';
+        private const char Left = 'L';
+        private const char Up = 'U';
+        private const char Down = 'D';
+        private List<(int, int)> Trace { get; }
         public List<(int, int)> Vertical { get; }
         public List<(int, int)> Horizontal { get; }
-        public List<(int, int)> Trace { get; }
+
 
         public Wire()
         {
             Tip = _centralPort;
             Horizontal = new List<(int, int)>();
             Vertical = new List<(int, int)>();
-            Trace = new List<(int,int)>();
+            Trace = new List<(int, int)>();
         }
 
         public void Parse(string[] traces)
         {
             foreach (var trace in traces)
             {
-                if (trace.StartsWith("R"))
+                switch (trace.First())
                 {
-                    var length = int.Parse(trace.Substring(1, trace.Length - 1));
-                    for (var i = 1; i <= length; i++)
-                    {
-                        var point = (Tip.Item1 + i, Tip.Item2);
-
-                        Trace.Add(point);
-                        Horizontal.Add(point);
-                    }
-                    Tip = Horizontal.Last();
-                }
-
-                if (trace.StartsWith("L"))
-                {
-                    var length = int.Parse(trace.Substring(1, trace.Length - 1));
-                    for (var i = 1; i <= length; i++)
-                    {
-                        var point = (Tip.Item1 - i, Tip.Item2);
-
-                        Trace.Add(point);
-                        Horizontal.Add(point);
-                    }
-                    Tip = Horizontal.Last();
-                }
-
-                if (trace.StartsWith("U"))
-                {
-                    var length = int.Parse(trace.Substring(1, trace.Length - 1));
-                    for (var i = 1; i <= length; i++)
-                    {
-                        var point = (Tip.Item1, Tip.Item2 + i);
-
-                        Trace.Add(point);
-                        Vertical.Add(point);
-                    }
-                    Tip = Vertical.Last();
-                }
-
-                if (trace.StartsWith("D"))
-                {
-                    var length = int.Parse(trace.Substring(1, trace.Length - 1));
-                    for (var i = 1; i <= length; i++)
-                    {
-                        var point = (Tip.Item1, Tip.Item2 - i);
-
-                        Trace.Add(point);
-                        Vertical.Add(point);
-                    }
-                    Tip = Vertical.Last();
+                    case Right:
+                        TraceRight(trace);
+                        break;
+                    case Left:
+                        TraceLeft(trace);
+                        break;
+                    case Up:
+                        TraceUp(trace);
+                        break;
+                    case Down:
+                        TraceDown(trace);
+                        break;
+                    default: throw new Exception("Unknown trace direction");
                 }
             }
+        }
+
+        private void TraceDown(string trace)
+        {
+            var length = int.Parse(trace.Substring(1, trace.Length - 1));
+            for (var i = 1; i <= length; i++)
+            {
+                var point = (Tip.Item1, Tip.Item2 - i);
+
+                Trace.Add(point);
+                Vertical.Add(point);
+            }
+
+            Tip = Vertical.Last();
+        }
+
+        private void TraceUp(string trace)
+        {
+            var length = int.Parse(trace.Substring(1, trace.Length - 1));
+            for (var i = 1; i <= length; i++)
+            {
+                var point = (Tip.Item1, Tip.Item2 + i);
+
+                Trace.Add(point);
+                Vertical.Add(point);
+            }
+
+            Tip = Vertical.Last();
+        }
+
+        private void TraceLeft(string trace)
+        {
+            var length = int.Parse(trace.Substring(1, trace.Length - 1));
+            for (var i = 1; i <= length; i++)
+            {
+                var point = (Tip.Item1 - i, Tip.Item2);
+
+                Trace.Add(point);
+                Horizontal.Add(point);
+            }
+
+            Tip = Horizontal.Last();
+        }
+
+        private void TraceRight(string trace)
+        {
+            var length = int.Parse(trace.Substring(1, trace.Length - 1));
+            for (var i = 1; i <= length; i++)
+            {
+                var point = (Tip.Item1 + i, Tip.Item2);
+
+                Trace.Add(point);
+                Horizontal.Add(point);
+            }
+
+            Tip = Horizontal.Last();
         }
 
         public IEnumerable<(int, int)> FindIntersections(Wire anotherWire)
@@ -97,7 +123,7 @@ namespace AdventOfCode2019.DayThree
                 .OrderBy(x => x)
                 .First();
 
-        public int FindMinStepsIntersection(Wire anotherWire) => 
+        public int FindMinStepsIntersection(Wire anotherWire) =>
             FindIntersections(anotherWire)
                 .Select(x => FindStepsTo(x) + anotherWire.FindStepsTo(x))
                 .Min();
