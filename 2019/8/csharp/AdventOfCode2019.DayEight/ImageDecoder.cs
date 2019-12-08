f2ceccc87039a1f8d527711afb21789cfea7039b
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-08 18:36:44 +0000

    Day8-c#: Add solution for the day

---
+++ 2019/8/csharp/AdventOfCode2019.DayEight/ImageDecoder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2019.DayEight
+{
+    public class ImageDecoder
+    {
+        private readonly string _encrypted;
+        private readonly int _width;
+        private readonly int _height;
+
+        public ImageDecoder(string encrypted, int width, int height)
+        {
+            _encrypted = encrypted;
+            _width = width;
+            _height = height;
+        }
+
+        public string Decode()
+        {
+            var layerSize = _width * _height;
+            var message = new List<char>();
+
+            for (var j = 0; j < layerSize; j++)
+            {
+                for (var i = 0; i < _encrypted.Length; i += layerSize)
+                {
+                    if (_encrypted[i + j] == '2') continue;
+                    message.Add(_encrypted[i + j]);
+                    break;
+                }
+            }
+
+            return new string(message.ToArray());
+        }
+
+        public void WriteMessage()
+        {
+            var message = Decode();
+
+            for (int i = 0; i < message.Length; i += _width)
+            {
+                File.AppendAllLines("output", new[] { message.Substring(i, _width).Replace('0',' ').Replace('1', '@') });
+            }
+        }
+    }
+}
---
+++ 2019/8/csharp/AdventOfCode2019.DayEightTests/SpaceImageFormatDecoderTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode2019.DayEight;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayEightTests
+{
+    public class SpaceImageFormatDecoderTests
+    {
+        [Fact]
+        public void PartOne()
+        {
+            var image = File.ReadAllText("input");
+            var layers = new List<string>();
+            var width = 25;
+            var height = 6;
+            var layerSize = width * height;
+
+            for (var layer = 0; layer < image.Length; layer += layerSize)
+            {
+                layers.Add(image.Substring(layer, layerSize));
+            }
+
+            var layersWithMinZeros = layers.GroupBy(g => g.Count(x => x == '0'), x => x).Where(x => x.Key > 0).OrderBy(x => x.Key).First();
+            var onesCount = layersWithMinZeros.Single().Count(x => x == '1');
+            var twosCount = layersWithMinZeros.Single().Count(x => x == '2');
+
+            (onesCount * twosCount).Should().Be(1572);
+        }
+
+        [Theory]
+        [InlineData("0222112222120000", 2, 2, "0110")]
+        public void TestPartTwo(string image, int width, int height, string result)
+            => new ImageDecoder(image, width, height).Decode().Should().Be(result);
+
+        [Fact]
+        public void PartTwoSolution()
+        {
+            var encrypted = File.ReadAllText("input");
+            var decoder = new ImageDecoder(encrypted, 25, 6);
+
+            decoder.WriteMessage();
+            //KYHFE
+        }
+
+    }
+}
