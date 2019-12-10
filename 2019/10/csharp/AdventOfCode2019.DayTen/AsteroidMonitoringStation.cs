bc21b8c47cc18fbf1f78531daa5628c0ac732749
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-12-10 22:48:50 +0000

    Day10.1-c#: Add solution for part one

---
+++ 2019/10/csharp/AdventOfCode2019.DayTen/AsteroidMonitoringStation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2019.DayTen
+{
+    public class AsteroidMonitoringStation
+    {
+        public IReadOnlyCollection<(int X, int Y)> Asteroids { get; }
+
+        public AsteroidMonitoringStation(string[] map)
+        {
+            Asteroids = GetAsteroids(map);
+        }
+
+        private static IReadOnlyCollection<(int, int)> GetAsteroids(IEnumerable<string> map) =>
+            map.SelectMany((line, y) => line
+                    .Select((legend, x) => (X: x, Y:y, Legend: legend))
+                    .Where(x => x.Legend == '#')
+                    .Select(asteroid => (asteroid.X, asteroid.Y)))
+                .ToList();
+
+        public IReadOnlyCollection<(int X, int Y)> DetectAsteroidsInSight((int X, int Y) station)
+        {
+            return Asteroids
+                .Where(asteroid => asteroid != station)
+                .GroupBy(asteroid => LineOfSight(asteroid, station))
+                .Select(asteroidByDistance =>
+                    asteroidByDistance.Select(asteroidInLine => new
+                        {
+                            asteroidInLine,
+                            Distance = Distance(station, asteroidInLine)
+                        })
+                        .OrderBy(asteroid => asteroid.Distance)
+                        .Select(directSight => directSight.asteroidInLine)
+                        .First())
+                .ToList();
+
+            int Distance((int X, int Y) a, (int X, int Y) b) => Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+            double LineOfSight((int X, int Y) a, (int X, int Y) b) => Math.Atan2(a.X - b.X, a.Y - b.Y);
+        }
+
+        public (int X, int Y) FindLocationWithMostAsteroidsInDirectSight() =>
+            Asteroids
+                .Select(a => new { Station = a, DetectAsteroidsInSight(a).Count })
+                .OrderByDescending(asteroids => asteroids.Count)
+                .First().Station;
+    }
+}
---
+++ 2019/10/csharp/AdventOfCode2019.DayTenTests/AsteroidMonitoringStationTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using AdventOfCode2019.DayTen;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayTenTests
+{
+    public class AsteroidMonitoringStationTests
+    {
+        [Theory]
+        [InlineData(".", 0)]
+        [InlineData("..", 0)]
+        [InlineData(@"..
+..", 0)]
+        [InlineData(@"#", 1)]
+        [InlineData(@"##", 2)]
+        [InlineData(@"##
+#.", 3)]
+        public void ShouldHaveCorrectCountOfAsteroidsOnMap(string map, int asteroidsCount)
+        {
+            var m = map.Split(Environment.NewLine);
+
+            new AsteroidMonitoringStation(m).Asteroids.Should().HaveCount(asteroidsCount);
+        }
+
+        [Theory]
+        [InlineData("#", 0, 0)]
+        [InlineData(".#", 1, 0)]
+        [InlineData(@"..
+#.", 0, 1)]
+        [InlineData(@"..
+.#", 1, 1)]
+        public void ShouldHaveCorrectAsteroidCoordinates(string map, int x, int y)
+        {
+            var m = map.Split(Environment.NewLine);
+
+            new AsteroidMonitoringStation(m).Asteroids.Should().ContainSingle(asteroid => asteroid.X == x && asteroid.Y == y);
+        }
+
+        [Theory]
+        [InlineData(@"###
+.##
+...", 1, 0, 4)]
+        [InlineData(@".#..#
+.....
+#####
+....#
+...##", 3, 4, 8)]
+        [InlineData(@".#..#
+.....
+#####
+....#
+...##", 3, 4, 8)]
+        [InlineData(@"......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####", 5, 8, 33)]
+        [InlineData(@"#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.", 1, 2, 35)]
+        [InlineData(@".#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..", 6, 3, 41)]
+        [InlineData(@".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##", 11, 13, 210)]
+        public void ShouldDetectAsteroidsInDirectLineOfSight(string map, int x, int y, int detectedAsteroidsCount)
+        {
+            var m = map.Split(Environment.NewLine);
+            var expectedStation = (x, y);
+
+            var asteroidMonitoringStation = new AsteroidMonitoringStation(m);
+
+            asteroidMonitoringStation.DetectAsteroidsInSight((x, y)).Should().HaveCount(detectedAsteroidsCount);
+
+            asteroidMonitoringStation.FindLocationWithMostAsteroidsInDirectSight().Should().Be(expectedStation);
+        }
+
+        [Fact]
+        public void SolutionForPartOne()
+        {
+            var map = @"
+#....#.....#...#.#.....#.#..#....#
+#..#..##...#......#.....#..###.#.#
+#......#.#.#.....##....#.#.....#..
+..#.#...#.......#.##..#...........
+.##..#...##......##.#.#...........
+.....#.#..##...#..##.....#...#.##.
+....#.##.##.#....###.#........####
+..#....#..####........##.........#
+..#...#......#.#..#..#.#.##......#
+.............#.#....##.......#...#
+.#.#..##.#.#.#.#.......#.....#....
+.....##.###..#.....#.#..###.....##
+.....#...#.#.#......#.#....##.....
+##.#.....#...#....#...#..#....#.#.
+..#.............###.#.##....#.#...
+..##.#.........#.##.####.........#
+##.#...###....#..#...###..##..#..#
+.........#.#.....#........#.......
+#.......#..#.#.#..##.....#.#.....#
+..#....#....#.#.##......#..#.###..
+......##.##.##...#...##.#...###...
+.#.....#...#........#....#.###....
+.#.#.#..#............#..........#.
+..##.....#....#....##..#.#.......#
+..##.....#.#......................
+.#..#...#....#.#.....#.........#..
+........#.............#.#.........
+#...#.#......#.##....#...#.#.#...#
+.#.....#.#.....#.....#.#.##......#
+..##....#.....#.....#....#.##..#..
+#..###.#.#....#......#...#........
+..#......#..#....##...#.#.#...#..#
+.#.##.#.#.....#..#..#........##...
+....#...##.##.##......#..#..##....";
+
+            var asteroidMonitoringStation = new AsteroidMonitoringStation(map.Split(Environment.NewLine));
+
+            var stationLocation = asteroidMonitoringStation.FindLocationWithMostAsteroidsInDirectSight();
+
+            stationLocation.Should().Be((26, 29));
+
+            asteroidMonitoringStation.DetectAsteroidsInSight(stationLocation).Should().HaveCount(267);
+        }
+    }
+}
