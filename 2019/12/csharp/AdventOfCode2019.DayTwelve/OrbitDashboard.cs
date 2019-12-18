6ec4641f4f4b8031c090316330793c50c61efcaf
Sakis Koltsidas <sakis.koltsidas@gmail.com>
2019-12-18 10:49:40 +0000

    Day12.1-c#: Add OrbitDashboard to parse initial moon positions with velocities

---
+++ 2019/12/csharp/AdventOfCode2019.DayTwelve/OrbitDashboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2019.DayTwelve
+{
+    public class OrbitDashboard
+    {
+        public IReadOnlyCollection<(int X, int Y, int Z)> Positions { get; }
+        public IReadOnlyCollection<(int X, int Y, int Z)> Velocities { get; }
+
+        private readonly Regex _moonPositionRegex = new Regex(@"<x=(?'X'-?\d*), y=(?'Y'-?\d*), z=(?'Z'-?\d*)>", RegexOptions.Multiline);
+
+        public OrbitDashboard(string moonStartingPositions)
+        {
+            Positions = _moonPositionRegex
+                .Matches(moonStartingPositions)
+                .Cast<Match>()
+                .Select(x => (
+                    X: int.Parse(x.Groups["X"].Value),
+                    Y: int.Parse(x.Groups["Y"].Value),
+                    Z: int.Parse(x.Groups["Z"].Value)))
+                .ToList();
+
+            Velocities = Positions.Select(x => (X: 0, Y: 0, Z: 0)).ToList();
+        }
+    }
+}
---
+++ 2019/12/csharp/AdventOfCode2019.DayTwelveTests/OrbitDashboardTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using AdventOfCode2019.DayTwelve;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayTwelveTests
+{
+    public class OrbitDashboardTests
+    {
+        [Fact]
+        public void ShouldInitializeOrbitDashboardWithCorrectInitialMoonPositionsAndVelocities()
+        {
+            const string moonStartingPositions = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            var expected = new List<(int, int, int)>
+            {
+                (-1,0,2),
+                (2,-10,-7),
+                (4,-8,8),
+                (3,5,-1)
+            };
+
+            var orbitDashboard = new OrbitDashboard(moonStartingPositions);
+
+            orbitDashboard.Positions.Should().HaveCount(4);
+            orbitDashboard.Positions.Should().BeEquivalentTo(expected);
+
+            orbitDashboard.Velocities.Should().HaveCount(4);
+            orbitDashboard.Velocities.Should().AllBeEquivalentTo((0,0,0));
+        }
+    }
+}
