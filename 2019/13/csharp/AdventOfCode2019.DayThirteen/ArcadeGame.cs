e801363a24b943c5ae7fa07046000543730c6f92
Sakis Koltsidas <sakis.koltsidas@gmail.com>
2019-12-27 16:43:32 +0000

    Day13-c#: Add part one solution

---
+++ 2019/13/csharp/AdventOfCode2019.DayThirteen/ArcadeGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2019.DayFive;
+
+namespace AdventOfCode2019.DayThirteen
+{
+    public class ArcadeGame
+    {
+        private readonly IntcodeComputer _computer;
+
+        public ArcadeGame(string gameCode)
+        {
+            _computer = IntcodeComputer.Create(gameCode, maxMemory: 4096);
+        }
+
+        public void Start()
+        {
+            _computer.ProcessToEnd();
+        }
+
+        public bool Finished => _computer.Completed;
+
+        public IEnumerable<(long X, long Y, long Id)> Grid => _computer.Output
+            .Select((x,i) => new {Output = x, Index = i})
+            .GroupBy(chunk => chunk.Index / 3)
+            .Select(c => c.Where(x => x.Output.HasValue).Select(x => x.Output.Value).ToArray())
+            .Select(tile => (X: tile[0], Y: tile[1], Id: tile[2]));
+
+        public IEnumerable<(long X, long Y, long Id)> Blocks => Grid.Where(tile => tile.Id == 2);
+    }
+}
---
+++ 2019/13/csharp/AdventOfCode2019.DayThirteenTests/ArcadeGameTestsWhenGameStarts.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using AdventOfCode2019.DayThirteen;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayThirteenTests
+{
+    public class ArcadeGameTestsWhenGameStarts
+    {
+        private readonly ArcadeGame _arcadeGame;
+
+        public ArcadeGameTestsWhenGameStarts()
+        {
+            var gameCode = File.ReadAllText("input");
+            _arcadeGame = new ArcadeGame(gameCode);
+            _arcadeGame.Start();
+        }
+
+        [Fact]
+        public void GridShouldNotBeEmpty() => _arcadeGame.Grid.Should().NotBeEmpty();
+
+        [Fact]
+        public void GridShouldHaveCorrectNumberOfBlockTiles() => _arcadeGame.Blocks.Should().HaveCount(253);
+    }
+}
