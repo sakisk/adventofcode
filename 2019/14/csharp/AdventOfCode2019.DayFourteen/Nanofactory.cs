3f9cf203cf306dabd63885b462fe8bb38cda1949
Sakis Koltsidas <sakis.koltsidas@gmail.com>
2020-01-07 01:24:33 +0000

    Day14-c#: Add part one solution

---
+++ 2019/14/csharp/AdventOfCode2019.DayFourteen/Nanofactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2019.DayFourteen
+{
+    public class Nanofactory
+    {
+        public IEnumerable<((long Quantity, string Chemical) Output, IEnumerable<(long Quantity, string Chemical)> Ingredients)> FuelRecipe { get; }
+
+        private readonly Regex _ingredientRegex = new Regex(@"(?'quantity'\d+) (?'chemical'[A-Z]+)");
+        public Nanofactory(string reactions) => FuelRecipe = ParseReactions(reactions);
+
+        private IEnumerable<((long Quantity, string Chemical) Output, IEnumerable<(long Quantity, string Chemical)> Ingredients)> ParseReactions(string reactions)
+        {
+            foreach (var reaction in reactions.Split(Environment.NewLine))
+            {
+                var parts = reaction.Split("=>").Select(x => x.Trim()).ToList();
+                var result = _ingredientRegex.Match(parts.Last());
+                var ingredients = _ingredientRegex.Matches(parts.First());
+
+                yield return (Output: ParseIngredients(result), Ingredients: ingredients.Select(ParseIngredients));
+            }
+
+            static (long Quantity, string Chemical) ParseIngredients(Match reaction) =>
+            (
+                Quantity: long.Parse(reaction.Groups["quantity"].Value),
+                Chemical: reaction.Groups["chemical"].Value
+            );
+        }
+
+        public long React()
+        {
+            var ore = 0L;
+            var reactor = new Queue<(long Quantity, string Name)>(new[] {FuelRecipe.Single(x => x.Output.Chemical == "FUEL").Output});
+            var leftOvers = FuelRecipe.ToDictionary(x => x.Output.Chemical, _ => 0L);
+
+            while (reactor.Any())
+            {
+                var (quantity, chemical) = reactor.Dequeue();
+                if (chemical == "ORE")
+                    ore += quantity;
+                else
+                {
+                    var leftOver = Math.Min(quantity, leftOvers[chemical]);
+                    quantity -= leftOver;
+                    leftOvers[chemical] -= leftOver;
+                    if (quantity > 0)
+                    {
+                        var ((outputQuantity, _), ingredients) = FuelRecipe.Single(x => x.Output.Chemical == chemical);
+                        var multiplier = (int) Math.Ceiling((decimal) quantity / outputQuantity);
+                        leftOvers[chemical] = Math.Max(0L, multiplier * outputQuantity - quantity);
+                        foreach (var (q, c) in ingredients)
+                            reactor.Enqueue((q * multiplier, c));
+                    }
+                }
+            }
+
+            return ore;
+        }
+    }
+}
---
+++ 2019/14/csharp/AdventOfCode2019.DayFourteenTests/NanofactoryTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2019.DayFourteen;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2019.DayFourteenTests
+{
+    public class NanofactoryTestsParsingReactions
+    {
+        [Theory]
+        [MemberData(nameof(Reactions))]
+        public void ShouldParseChemicalsForASingleReactionCorrectly(string input, (int Quantity, string Chemical) result, (int Quantity, string Chemical)[] ingredients)
+        {
+            var reactions = new Nanofactory(input).FuelRecipe.ToList();
+            reactions.Should().ContainSingle(x => x.Output.Quantity == result.Quantity && x.Output.Chemical == result.Chemical);
+            reactions.Single().Ingredients.Should().BeEquivalentTo(ingredients);
+        }
+
+        public static IEnumerable<object[]> Reactions => new[]
+        {
+            new object[]
+            {
+                "10 ORE => 10 A", (Quantity: 10, Name: "A"), new[]
+                {
+                    (Quantity: 10, Name: "ORE")
+                }
+            },
+            new object[]
+            {
+                "7 A, 1 B => 1 C", (Quantity: 1, Name: "C"), new[]
+                {
+                    (Quantity: 1, Name: "B"),
+                    (Quantity: 7, Name: "A"),
+                }
+            }
+        };
+    }
+}
